9fccbf6a5d7e0c00ac25a0f4ff70e95ff3c8ac4f
Blair Archibald <mrblairarchibald@gmail.com>
2017-09-20 19:45:44 +0100

    Remove spinlock from position index

--- lib/util/positionIndex.hpp
+++ lib/util/positionIndex.hpp
@@ -2,21 +2,19 @@
 #define POSITION_INDEX_HPP
 
 #include <vector>
-#include <mutex>
-#include <hpx/hpx.hpp>
-#include <hpx/util/spinlock.hpp>
-#include <boost/serialization/access.hpp>
+#include <hpx/lcos/future.hpp>
+#include <hpx/util/tuple.hpp>
+#include <boost/optional.hpp>
 
 class positionIndex {
-private:
+ private:
   unsigned depth;
   std::vector<unsigned> path;
   std::vector<int> children;
   std::vector<int> nextIndex;
   std::vector<hpx::future<void> > futures;
-  hpx::util::spinlock mtx;
 
-public:
+ public:
 
   positionIndex() : depth(0) {
     path.reserve(30);
@@ -48,16 +46,13 @@ public:
       children.push_back(-1);
       nextIndex.push_back(-1);
     }
-
   }
 
   std::vector<unsigned> getPath() {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     return path;
   }
 
   void setNumChildren(int numChildren) {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     children[depth]  = numChildren;
     if (numChildren > 0) {
       nextIndex[depth] = 0;
@@ -65,7 +60,6 @@ public:
   }
 
   void pruneLevel() {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     children[depth] = 0;
   }
 
@@ -73,7 +67,6 @@ public:
   // Essentially gives ownership of this node to the caller
   // Return -1 if there is no next position
   int getNextPosition() {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     if (children[depth] == 0) {
       return -1;
     } else {
@@ -85,7 +78,6 @@ public:
   }
 
   void preExpand(unsigned i) {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     path.push_back(i);
     children.push_back(-1);
     nextIndex.push_back(-1);
@@ -93,19 +85,14 @@ public:
   }
 
   void postExpand() {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
     depth--;
     path.pop_back();
     children.pop_back();
     nextIndex.pop_back();
   }
 
-  std::pair<std::vector<unsigned>, hpx::naming::id_type> steal() {
-    std::lock_guard<hpx::util::spinlock> l(mtx);
-
+  boost::optional<hpx::util::tuple<std::vector<unsigned>, int, hpx::naming::id_type> > steal() {
     // Find the highest depth that still has work
-    std::vector<unsigned> res;
-
     int highest = -1;
     for (auto i = 0; i <= depth; ++i) {
       if (children[i] > 0) {
@@ -115,37 +102,35 @@ public:
     }
 
     if (highest == -1) {
-      return std::make_pair(res, hpx::find_here()); //Nothing to steal
-    } else {
-      children[highest]--;
-
-      // Calc the path
-      for (auto i = 0; i <= highest; ++i) {
-        res.push_back(path[i]);
-      }
-      auto idx = nextIndex[highest];
-      nextIndex[highest]++;
-      res.push_back(idx);
-
-      hpx::promise<void> prom;
-      auto f = prom.get_future();
-      auto pid = prom.get_id();
-
-      futures.push_back(std::move(f));
-
-      return std::make_pair(res, pid);
+      return {};
     }
+
+    // Take this child
+    children[highest]--;
+
+    // Calc the path
+    std::vector<unsigned> res;
+    for (auto i = 0; i <= highest; ++i) {
+      res.push_back(path[i]);
+    }
+
+    auto idx = nextIndex[highest];
+    nextIndex[highest]++;
+    res.push_back(idx);
+
+    hpx::promise<void> prom;
+    auto f = prom.get_future();
+    auto pid = prom.get_id();
+
+    futures.push_back(std::move(f));
+
+    // FIXME: Should path size be returned here?
+    return hpx::util::make_tuple(std::move(res), res.size(), pid);
   }
 
   void waitFutures() {
-    // We don't want to hold the lock here. Only called once we are done
-    // everything else so no steals will succeed and no one else can modify the
-    // index so this is safe
     hpx::wait_all(futures);
   }
-
-  template <class Archive>
-  void serialize(Archive & ar, const unsigned int version) {}
 };
 
 #endif
