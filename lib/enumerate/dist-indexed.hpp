42f7907b2dcfe005c3dd6921a7b931b85485d03e
Blair Archibald <mrblairarchibald@gmail.com>
2017-12-11 14:04:05 +0000

    Update recompute based skeleton with new SearchManager

--- lib/enumerate/dist-indexed.hpp
+++ lib/enumerate/dist-indexed.hpp
@@ -20,15 +20,10 @@ namespace skeletons { namespace Enum {
 
 template <typename Space, typename Sol, typename Gen>
 struct DistCount<Space, Sol, Gen, Indexed> {
-  using Response_t    = std::vector<hpx::util::tuple<std::vector<unsigned>, int, hpx::naming::id_type> >;
-  using SharedState_t = std::tuple<std::atomic<bool>, hpx::lcos::local::one_element_channel<Response_t>, bool >;
 
   static void searchChildTask(std::vector<unsigned> path,
                               const int depth,
-                              std::shared_ptr<SharedState_t> stealRequest,
-                              const hpx::naming::id_type doneProm,
-                              const int idx,
-                              const hpx::naming::id_type searchMgr) {
+                              const hpx::naming::id_type doneProm) {
     auto reg = Registry<Space, Sol>::gReg;
 
     auto posIdx = positionIndex(path);
@@ -37,12 +32,15 @@ struct DistCount<Space, Sol, Gen, Indexed> {
     std::vector<std::uint64_t> cntMap;
     cntMap.resize(reg->maxDepth + 1);
 
+    std::shared_ptr<SharedState_t> stealRequest; unsigned idx;
+    std::tie(stealRequest, idx) = std::static_pointer_cast<Policy_t>(Workstealing::Scheduler::local_policy)->registerThread();
+
     expand(posIdx, reg->maxDepth, depth, c, stealRequest, cntMap);
+
+    std::static_pointer_cast<Policy_t>(Workstealing::Scheduler::local_policy)->unregisterThread(idx);
 
     // Atomically updates the (process) local counter
     reg->updateCounts(cntMap);
-
-    std::static_pointer_cast<Workstealing::Policies::SearchManager<std::vector<unsigned>, ChildTask> >(Workstealing::Scheduler::local_policy)->done(idx);
 
     hpx::apply([=](auto pIdx) {
           pIdx.waitFutures();
@@ -53,6 +51,10 @@ struct DistCount<Space, Sol, Gen, Indexed> {
   using ChildTask = func<
     decltype(&DistCount<Space, Sol, Gen, Indexed>::searchChildTask),
     &DistCount<Space, Sol, Gen, Indexed>::searchChildTask>;
+
+  using Policy_t      = Workstealing::Policies::SearchManager<std::vector<unsigned>, ChildTask>;
+  using Response_t    = typename Policy_t::Response_t;
+  using SharedState_t = typename Policy_t::SharedState_t;
 
   static void expand(positionIndex & pos,
                      const unsigned maxDepth,
@@ -106,8 +108,12 @@ struct DistCount<Space, Sol, Gen, Indexed> {
 
     Workstealing::Policies::SearchManager<std::vector<unsigned>, ChildTask>::initPolicy(stealAll);
 
-    auto threadCount = hpx::get_os_thread_count() - 1;
-    hpx::wait_all(hpx::lcos::broadcast<Workstealing::Scheduler::startSchedulers_act>(hpx::find_all_localities(), threadCount));
+    auto distributedThreads = hpx::get_os_thread_count() == 1 ? 1 : hpx::get_os_thread_count() - 1;
+    hpx::wait_all(hpx::lcos::broadcast<Workstealing::Scheduler::startSchedulers_act>(util::findOtherLocalities(), distributedThreads));
+
+    // Start N-2 local schedulers since we start another below
+    auto localThreads = hpx::get_os_thread_count() < 3 ? 0 : hpx::get_os_thread_count() - 2;
+    hpx::async<Workstealing::Scheduler::startSchedulers_act>(hpx::find_here(), localThreads).get();
 
     // Push the root node as a task to the searchManager
     std::vector<unsigned> path;
@@ -117,7 +123,10 @@ struct DistCount<Space, Sol, Gen, Indexed> {
     auto f = prom.get_future();
     auto pid = prom.get_id();
 
-    std::static_pointer_cast<Workstealing::Policies::SearchManager<std::vector<unsigned>, ChildTask> >(Workstealing::Scheduler::local_policy)->addWork(path, 1, pid);
+    // Start off the main scheduler
+    hpx::threads::executors::default_executor exe(hpx::threads::thread_priority_critical,
+                                                  hpx::threads::thread_stacksize_huge);
+    hpx::apply(exe, &Workstealing::Scheduler::scheduler, hpx::util::bind(&searchChildTask, path, 1, pid));
 
     // Wait completion of the main task
     f.get();
--- lib/util/util.hpp
+++ lib/util/util.hpp
@@ -1,10 +1,20 @@
 #ifndef YEWPAR_UTIL_HPP
 #define YEWPAR_UTIL_HPP
 
+#include <vector>
+
 namespace util {
 
 bool isColocated(hpx::naming::id_type & id) {
   return hpx::get_colocation_id(hpx::launch::sync, id) == hpx::find_here();
+}
+
+// Find all localities except the one the function is called on
+std::vector<hpx::naming::id_type> findOtherLocalities () {
+  auto locs = hpx::find_all_localities();
+  auto here = hpx::find_here();
+  locs.erase(std::remove(locs.begin(), locs.end(), here), locs.end());
+  return locs;
 }
 
 }
