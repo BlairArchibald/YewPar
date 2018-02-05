885b9be91790d50eac700bfcc243220aec0bc2a5
Blair Archibald <mrblairarchibald@gmail.com>
2018-02-05 13:50:50 +0000

    Fix nix derivation for updated nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,9 @@
 with import <nixpkgs> {};
+let
+stdenv = overrideCC pkgs.stdenv pkgs.gcc7;
+boost = boost164;
 
-let newHPX = pkgs.hpx.overrideDerivation (oldAttrs: {
+newHPX = stdenv.mkDerivation rec {
   name = "hpx-new";
   src = fetchFromGitHub {
     owner = "STEllAR-GROUP";
@@ -9,7 +12,8 @@ let newHPX = pkgs.hpx.overrideDerivation (oldAttrs: {
     sha256 = "1bydsxsblgrqykw4rw50qmlbqn7q4msirfbcw3y9dv631svw34k5";
   };
 
-  buildInputs = oldAttrs.buildInputs ++ [ openmpi gcc7 ];
+  buildInputs = [ boost mpich2 hwloc gperftools];
+  nativeBuildInputs = [ cmake pkgconfig python ];
 
   cmakeFlags = [
     "-DHPX_WITH_PARCELPORT_MPI=ON"
@@ -17,20 +21,22 @@ let newHPX = pkgs.hpx.overrideDerivation (oldAttrs: {
     "-DHPX_WITH_TESTS=OFF"
   ];
 
-});
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "C++ standard library for concurrency and parallelism";
+    homepage = https://github.com/STEllAR-GROUP/hpx;
+    license = stdenv.lib.licenses.boost;
+    platforms = [ "x86_64-linux" ]; # stdenv.lib.platforms.linux;
+  };
+};
 in
 stdenv.mkDerivation rec {
   name = "YewPar";
   version = "0.0.1";
 
-  buildInputs = [
-    gcc7
-    boost
-    openmpi
-    newHPX
-    gperftools
-    hwloc
-  ];
+  buildInputs = [ boost newHPX ];
+  nativeBuildInputs = [ cmake ];
 
   src =
     let
@@ -39,8 +45,6 @@ stdenv.mkDerivation rec {
         || inDir path ./apps
         || inDir path ./lib;
     in builtins.filterSource filter ./.;
-
-  nativeBuildInputs = [ cmake ];
 
   enableParallelBuilding = true;
 }
