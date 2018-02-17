bce50cd7befa755db960403548cab9a218808cf9
Blair Archibald <mrblairarchibald@gmail.com>
2018-02-17 11:06:44 +0000

    Ensure tc malloc is available for building YewPar

--- default.nix
+++ default.nix
@@ -36,7 +36,7 @@ stdenv.mkDerivation rec {
   name = "YewPar";
   version = "0.0.1";
 
-  buildInputs = [ boost newHPX ];
+  buildInputs = [ boost newHPX mpich2 gperftools hwloc ];
   nativeBuildInputs = [ cmake ];
 
   src =
