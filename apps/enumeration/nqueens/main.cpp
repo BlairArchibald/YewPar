be7b93818bb2867add94b3d68a6ac7b13bf92464
Blair Archibald <mrblairarchibald@gmail.com>
2019-11-06 10:31:23 +0000

    Add simple nqueens enumeration program
    As Enumeration currently counts the nodes at each depth, we get the
    wrong answer in the case of no solutions. Else, the answer is the number
    of nodes at depth = board size.
    
    Later we will change this for a monoid to count/store only valid
    solutions.

---
+++ apps/enumeration/nqueens/main.cpp
@@ -0,0 +1,165 @@
+// N-Queens problem; following [1]
+//
+// [1] Richards, Martin (1997). Backtracking Algorithms in MCPL using
+//     Bit Patterns and Recursion (Technical report).
+//     University of Cambridge Computer Laboratory. UCAM-CL-TR-433.#include <hpx/hpx_init.hpp>
+//
+// Based on code from Patrick Maier
+//
+
+#include <hpx/hpx_init.hpp>
+#include <hpx/include/iostreams.hpp>
+
+#include "YewPar.hpp"
+#include "skeletons/Seq.hpp"
+#include "skeletons/DepthBounded.hpp"
+#include "skeletons/StackStealing.hpp"
+#include "skeletons/Budget.hpp"
+
+// N-queens doesn't have a space
+struct Empty {};
+
+struct Node {
+    std::uint32_t all;
+    std::uint32_t ld;
+    std::uint32_t cols;
+    std::uint32_t rd;
+    std::uint32_t poss;
+
+    Node() : all(0), ld(0), cols(0), rd(0), poss(0) {};
+    Node(std::uint32_t all, std::uint32_t ld, std::uint32_t cols,
+         std::uint32_t rd, std::uint32_t poss)
+    : all(all), ld(ld), cols(cols), rd(rd), poss(poss) {};
+};
+
+namespace hpx { namespace serialization {
+  template<class Archive>
+  void serialize(Archive & ar, Node & x, const unsigned int version) {
+    ar & x.all;
+    ar & x.ld;
+    ar & x.cols;
+    ar & x.rd;
+    ar & x.poss;
+  }
+}}
+
+struct NodeGen : YewPar::NodeGenerator<Node, Empty> {
+  std::uint32_t all;
+  std::uint32_t poss;
+  std::uint32_t ld;
+  std::uint32_t cols;
+  std::uint32_t rd;
+
+  NodeGen(const Empty &, const Node & parent) :
+  all(parent.all), ld(parent.ld), cols(parent.cols)
+  , rd(parent.rd), poss(parent.poss) {
+    this->numChildren = __builtin_popcount(poss);
+  }
+
+  Node next() override {
+      auto bit = poss & -poss;
+      poss -= bit;
+
+      auto new_ld = (ld | bit) << 1;
+      auto new_cols = cols | bit;
+      auto new_rd = (rd | bit) >> 1;
+      auto newP = ~(new_ld | new_cols | new_rd) & all;
+
+      return Node (all, new_ld, new_cols, new_rd, newP);
+  }
+};
+
+int hpx_main(boost::program_options::variables_map & opts) {
+  auto spawnDepth = opts["spawn-depth"].as<unsigned>();
+  auto size = opts["size"].as<unsigned>();
+  auto skeleton   = opts["skeleton"].as<std::string>();
+
+  auto all = (1 << size) - 1;
+  Node root(all, 0, 0, 0, all);
+
+  auto start_time = std::chrono::steady_clock::now();
+
+  std::vector<std::uint64_t> counts;
+  if (skeleton == "seq") {
+    YewPar::Skeletons::API::Params<> searchParameters;
+    counts = YewPar::Skeletons::Seq<NodeGen,
+                                    YewPar::Skeletons::API::CountNodes,
+                                    YewPar::Skeletons::API::DepthLimited>
+             ::search(Empty(), root, searchParameters);
+  } else if (skeleton == "depthbounded") {
+    YewPar::Skeletons::API::Params<> searchParameters;
+    searchParameters.spawnDepth = spawnDepth;
+    counts = YewPar::Skeletons::DepthBounded<NodeGen,
+                                            YewPar::Skeletons::API::CountNodes,
+                                            YewPar::Skeletons::API::DepthLimited>
+             ::search(Empty(), root, searchParameters);
+  } else if (skeleton == "stacksteal"){
+    YewPar::Skeletons::API::Params<> searchParameters;
+    searchParameters.stealAll = static_cast<bool>(opts.count("chunked"));
+    counts = YewPar::Skeletons::StackStealing<NodeGen,
+                                              YewPar::Skeletons::API::CountNodes,
+                                              YewPar::Skeletons::API::DepthLimited>
+             ::search(Empty(), root, searchParameters);
+  } else if (skeleton == "budget"){
+    YewPar::Skeletons::API::Params<> searchParameters;
+    searchParameters.backtrackBudget = opts["backtrack-budget"].as<unsigned>();
+    counts = YewPar::Skeletons::Budget<NodeGen,
+                                       YewPar::Skeletons::API::CountNodes,
+                                       YewPar::Skeletons::API::DepthLimited>
+        ::search(Empty(), root, searchParameters);
+  } else {
+    hpx::cout << "Invalid skeleton type: " << skeleton << hpx::endl;
+    return hpx::finalize();
+  }
+
+  auto overall_time = std::chrono::duration_cast<std::chrono::milliseconds>
+                      (std::chrono::steady_clock::now() - start_time);
+
+  auto res = 0;
+  for (auto i = 0; i <= counts.size(); ++i) {
+    if (counts[i] == 0) {
+        break;
+    }
+    res = counts[i];
+    // hpx::cout << i << ": " << counts[i] << hpx::endl;
+  }
+  hpx::cout << "Warning results are incorrect for boards with 0 solutions (due to how nodes are currently counted)" << hpx::endl;
+  hpx::cout << "Solution for n = " << size <<  ": " << res << hpx::endl;
+
+  hpx::cout << "=====" << hpx::endl;
+  hpx::cout << "cpu = " << overall_time.count() << hpx::endl;
+
+  return hpx::finalize();
+}
+
+int main(int argc, char* argv[]) {
+  boost::program_options::options_description
+    desc_commandline("Usage: " HPX_APPLICATION_STRING " [options]");
+
+  desc_commandline.add_options()
+    ( "skeleton",
+      boost::program_options::value<std::string>()->default_value("seq"),
+      "Which skeleton to use: seq, depthbound, stacksteal, or budget"
+    )
+    ( "spawn-depth,d",
+      boost::program_options::value<unsigned>()->default_value(0),
+      "Depth in the tree to spawn until (for parallel skeletons only)"
+    )
+    ( "size,n",
+      boost::program_options::value<unsigned>()->default_value(8),
+      "Boards size"
+    )
+    ( "backtrack-budget,b",
+      boost::program_options::value<unsigned>()->default_value(500),
+      "Number of backtracks before spawning work"
+    )
+    ( "verbose,v",
+      boost::program_options::value<bool>()->default_value(false),
+      "Enable verbose output"
+    )
+    ("chunked", "Use chunking with stack stealing");
+
+  YewPar::registerPerformanceCounters();
+
+  return hpx::init(desc_commandline, argc, argv);
+}
