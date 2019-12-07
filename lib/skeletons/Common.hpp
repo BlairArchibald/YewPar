a76d8209e7c4e13a62f0603128180443c3d3abda
Ruairidh Macgregor <2250079m@student.gla.ac.uk>
2019-12-07 20:01:14 +0000

    ...

--- lib/skeletons/Common.hpp
+++ lib/skeletons/Common.hpp
@@ -72,18 +72,19 @@ static auto printTimes(const unsigned maxDepth) {
   auto timesVec = hpx::lcos::broadcast<GetTimesAct>(
     hpx::find_all_localities()).get();
 
+	std::uint64_t i;
   // Get the median from each, compute the L1 norm and compute the mean
   std::vector<std::uint64_t> sums;
   std::vector<std::vector<std::uint64_t> > vec(maxDepth + 1);
   for (const auto & times : timesVec) {
-    for (int i = 0; i <= maxDepth; i++) {
+    for (i = 0; i <= maxDepth; i++) {
       for (const auto & time : times[i]) {
         vec[i].push_back(time);
       }
     }
   }
 
-  for (int i = 0; i <= maxDepth; i++) {
+  for (i = 0; i <= maxDepth; i++) {
     int size = vec.size();
     std::sort(vec[i].begin(), vec[i].end());
     std::uint64_t median;
