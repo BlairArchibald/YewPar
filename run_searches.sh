d643ed8b8c280968edae1d166809aa81146ae989
ruairidh <ruairidh1998@icloud.com>
2019-12-16 14:03:26 +0000

    Updated script to change the name of the result file for each worker tested with

--- run_searches.sh
+++ run_searches.sh
@@ -2,7 +2,7 @@
 
 runSearches() {
 	for i in {1..5}; do
-		mpiexec -n $1 -f hostfile.txt ./build/install/bin/NS-hivert -g 48 --skel budget -b 10000000 --hpx:threads 4 >> results/NS-hivert_budget_search_metrics.txt
+		mpiexec -n $1 -f hostfile.txt ./build/install/bin/NS-hivert -g 48 --skel budget -b 10000000 >> results/NS-hivert_budget_search_metrics_$1.txt
 	done
 }
 
