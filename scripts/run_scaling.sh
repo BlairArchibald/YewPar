ba7292e9ab8ccfb95ff3d124c82825517e28bae9
Ruairidh Macgregor <2250079m@student.gla.ac.uk>
2020-01-19 09:57:19 +0000

    More results added

--- scripts/run_scaling.sh
+++ scripts/run_scaling.sh
@@ -9,61 +9,34 @@ MaxClique() {
 
   NPROCESSES=$1
 
-  for i in {1..5}; do
-    for j in {1..3}; do
-      mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f p_hat1500-$i.clq --skel depthbounded -d 2 >> max_clique_scaling_depthbounded.txt
+  for i in {1..3}; do
+      timeout 3600 mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f test/brock800_1.clq --skel depthbounded -d 2 --scaling >> max_clique_scaling_depthbounded_$1.txt
 
-      mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f p_hat1500-$i.clq --skel budget -b 1000000 >> max_clique_scaling_budget.txt
+      timeout 3600 mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f test/brock800_1.clq --skel budget -b 10000000 --scaling >> max_clique_scaling_budget_$1.txt
 
-      mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f p_hat1500-$i.clq --skel stacksteal --chunked >> max_clique_scaling_stacksteal.txt
-    done
+      timeout 3600 mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/maxclique-16 -f test/brock800_1.clq --skel stacksteal --scaling >> max_clique_scaling_stacksteal_$1.txt
   done
 
 }
 
-NSHivert() {
-
-  G=$1
-  D=$2
-  B=$3
-  NPROCESSES=$4
-  HOSTFILE=$5
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/NS-hivert -g $G --skel depthbounded -d $D >> ns-hivert_scaling_depthbounded.txt
+appendToHostFile() {
+  for i in $(seq $1 $2); do
+    echo 130.209.255.$i >> hostfile.txt
   done
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/NS-hivert -g $G --skel budget -b $B >> ns-hivert_scaling_budget.txt
-  done
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/NS-hivert -g $G --skel stacksteal --chunked >> ns-hivert_scaling_stacksteal.tzt
-  done
-
 }
 
+echo '130.209.255.4' > hostfile.txt 
+MaxClique 16
+appendToHostFile 5 5
+MaxClique 32
+appendToHostFile 6 7
+MaxClique 64
+appendToHostFile 8 11
+MaxClique 128
+appendToHostFile 12 19
+MaxClique 256
+appendToHostFile 20 20
+MaxClique 274
 
-Sip() {
 
-  PATTERN=$1
-  TARGET=$2
-  D=$3
-  B=$4
-  NPROCESSES=$5
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/sip --pattern $PATTERN --target $TARGET --skel depthbounded -d $D >> sip_scaling_depthbounded.txt
-  done
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/sip --pattern $PATTERN --target $TARGET --skel budget -b $B >> sip_scaling_budget.txt
-  done
-
-  for i in {1..5}; do
-    mpiexec -n $NPROCESSES -f $HOSTFILE ./build/install/bin/sip --pattern $PATTERN --target $TARGET --skel stacksteal --chunked >> sip_scaling_stacksteal.txt
-  done
-
-}
-
-MaxClique 
+exit 0
