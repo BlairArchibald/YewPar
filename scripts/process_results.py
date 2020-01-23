281720dbeae502657461668ec50ce7f5dad7172e
ruairidh <ruairidh1998@icloud.com>
2020-01-23 21:45:38 +0000

    Added max clique scaling results back to repo (temporarily)

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -35,7 +35,7 @@ def read_scaling_results(filename):
 
   return np.median(times), nodes
 
-def draw_scaling_graph(times, title, d, b, ylabel):
+def draw_scaling_graph(times, title, d, b, ylabel, incDep=True):
   """
   Draw speedup graph
   """
@@ -44,7 +44,8 @@ def draw_scaling_graph(times, title, d, b, ylabel):
   ax1.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
   x_axes = np.array([1, 2, 4, 8, 16, 17])
   ax1.plot(x_axes, times[1], marker="x", color="blue", label="Budget b = {}".format(b))
-  ax1.plot(x_axes, times[2], marker="x", color="red", label="DepthBounded d = {}".format(d))
+  if incDep:
+    ax1.plot(x_axes, times[2], marker="x", color="red", label="DepthBounded d = {}".format(d))
   ax1.plot(x_axes, times[0], marker="x", color="purple", label="StackSteal")
   ax1.set_ylabel(ylabel) 
   ax1.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
@@ -60,7 +61,7 @@ def get_speedups():
   """
   return (timesD[0]/timesD, timesB[0]/timesB, timesS[0]/timesS)
 
-def draw_bucket_graph(times, title):
+def draw_bucket_graph(times, title, axes):
   """
   Draws a bar chart for the runtime regularity
   """
@@ -117,9 +118,8 @@ def draw_bucket_graph(times, title):
   ax2.vlines(inds, whiskersMin, whiskersMax, color='k', linestyle='-', lw=1)
 
   # set style for the axes
-  labels = [0, 1, 2]
   for ax in [ax2]:
-      set_axis_style(ax, labels)
+      set_axis_style(ax, axes)
 
   plt.subplots_adjust(bottom=0.15, wspace=0.05)
   plt.show()
@@ -193,61 +193,44 @@ def draw_node_throughput(times, nodes, title, d, b):
   ax1.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
   ax1.legend()
   plt.show()
-"""
-times, nodes, backtracks, searchTimes, prunes = read_search_metrics("../rs_mc.txt")
-times2, nodes, backtracks, searchTimes, prunes = read_search_metrics("../rs_mc_1.txt")
-
-draw_bucket_graph(times2[:-1], "Runtime Regularity on MaxClique, Budget, b = 1000000, brock400_4.clq")
-
-times3, nodes, backtracks, searchTimes, prunes = read_search_metrics("../rs_mc3.txt")
-times4, nodes, backtracks, searchTimes, prunes = read_search_metrics("../rs_mc4.txt")"""
-"""times2, nodes2, backtracks, searchTimes2, prunes = read_search_metrics("../../../../Misc/MaxClique_depthbounded_search_metrics_175.txt")
-times3, nodes4, backtracks, searchTimes4, prunes = read_search_metrics("../../../../Misc/MaxClique_depthbounded_search_metrics_250.txt")"""
-
-"""
-times, nodes, backtracks, searchTimes, prunes = read_search_metrics("../../../../Misc/MaxClique_budget_search_metrics_100.txt")
-times2, nodes2, backtracks, searchTimes2, prunes = read_search_metrics("../../../../Misc/MaxClique_budget_search_metrics_175.txt")
-times3, nodes4, backtracks, searchTimes4, prunes = read_search_metrics("../../../../Misc/MaxClique_budget_search_metrics_250.txt")
-draw_bucket_graph(times, times2, times3, "Runtime Regularity on MaxClique, brock800_1.clq, Budget, b = 1000000, 100, 175 and 250 workers")
-
-times, nodes, backtracks, searchTimes, prunes = read_search_metrics("../../../../Misc/NS-hivert_budget2_search_metrics_100.txt")
-times2, nodes2, backtracks, searchTimes2, prunes = read_search_metrics("../../../../Misc/NS-hivert_budget2_search_metrics_150.txt")
-times3, nodes4, backtracks, searchTimes4, prunes = read_search_metrics("../../../../Misc/NS-hivert_budget2_search_metrics_250.txt")
-draw_bucket_graph(times, times2, times3, "Runtime Regularity on NS-hivert, g = 47, Budget, b = 10000, 100, 175 and 250 workers")
-
-times, nodes, backtracks, searchTimes, prunes = read_search_metrics("../results/Depthbounded/SIP_depthbounded_search_metrics_32.txt")
-print(backtracks)
-times2, nodes2, backtracks, searchTimes2, prunes = read_search_metrics("../results/Depthbounded/SIP_depthbounded_search_metrics_64.txt")
-print(backtracks)
-times3, nodes4, backtracks, searchTimes4, prunes = read_search_metrics("../results/Depthbounded/SIP_depthbounded_search_metrics_128.txt")
-print(backtracks)
-times4, nodes5, backtracks, searchTimes4, prunes = read_search_metrics("../results/Depthbounded/SIP_depthbounded_search_metrics_128.txt")
-print(backtracks)"""
-
-"""t, n, b, s, p = read_search_metrics("../results/SIP_budget_search_metrics_32.txt")
-print(s, n)
-t1, n1, b1, s1, p1 = read_search_metrics("../results/SIP_budget_search_metrics_50.txt")
-print(s1, n1)
-t2, n2, b2, s2, p2 = read_search_metrics("../results/SIP_budget_search_metrics_100.txt")
-print(s2, n2)
-t4, n4, b4, s4, p4 = read_search_metrics("../results/SIP_budget_search_metrics_250.txt")
-print(s4, n4)"""
-
-N = 2311090
-M = 21278925
-
-t_puts = [N/233, 15951030/386, 16455409/664, 17303299/938]
-t_s = [M/1534, 18650461/806, 17925657/837, 14784145/1017]
-#draw_node_throughput(t_puts, t_s, "Node throughput for SIP", 0, 100000)
 
 medians = np.zeros((3,6), dtype=np.float64)
 nodes = np.zeros((3,6), dtype=np.float64)
 ranges = [1, 2, 4, 8]#, 16, 17]
 j, o = 0, 0
-skels = ["stack_steal", "budget"]
+skels = ["stack_steal", "budget"]#, "depthbounded"]
 for i in ranges:
   for k in skels:
-    median, node = read_scaling_results("../ns_hivert_{}_{}_52.txt".format(k, i))
+    try:
+      median, node = read_scaling_results("../ns_hivert_{}_{}_52.txt".format(k, i))
+      medians[j,o] = np.float64(median)
+      nodes[j,o] = node
+    except:
+      j += 1
+      continue
+    j += 1
+  o += 1
+  j = 0
+
+speedUps = np.zeros(medians.shape, dtype=np.float64)
+throughPut = nodes / medians
+
+for i in range(6):
+  for j in range(3):
+    speedUps[j,i] = medians[j,0] / medians[j,i]
+
+draw_scaling_graph(medians, "Runtimes on NS-hivert, g = 52", 2, 100000, "Runtime (s)", incDep=False)
+draw_scaling_graph(speedUps, "Scaling on NS-hivert, g = 52", 2, 100000, "Relative SpeedUp (1 locality)", incDep=False)
+
+
+medians = np.zeros((3,6), dtype=np.float64)
+nodes = np.zeros((3,6), dtype=np.float64)
+ranges = [1, 2, 4, 8, 16, 17]
+j, o = 0, 0
+skels = ["stacksteal", "budget", "depthbounded"]
+for i in ranges:
+  for k in skels:
+    median, node = read_scaling_results("../max_clique_scaling_{}_{}.txt".format(k, i))
     medians[j,o] = np.float64(median)
     nodes[j,o] = node
     j += 1
@@ -263,5 +246,45 @@ for i in range(6):
 
 draw_scaling_graph(medians, "Runtimes on MaxClique on brock800_1.clq", 2, 10000000, "Runtime (s)")
 draw_scaling_graph(speedUps, "Scaling on MaxClique on brock800_1.clq", 2, 10000000, "Relative SpeedUp (1 locality)")
+
+medians = np.zeros((3,6), dtype=np.float64)
+nodes = np.zeros((3,6), dtype=np.float64)
+ranges = [1, 2]#, 4, 8, 16, 17]
+j, o = 0, 0
+skels = ["stacksteal", "budget", "depthbounded"]
+for i in ranges:
+  for k in skels:
+    median, node = read_scaling_results("../sip_scaling_{}_{}.txt".format(k, i))
+    medians[j,o] = np.float64(median)
+    nodes[j,o] = node
+    j += 1
+  o += 1
+  j = 0
+
+speedUps = np.zeros(medians.shape, dtype=np.float64)
+throughPut = nodes / medians
+
+for i in range(6):
+  for j in range(3):
+    speedUps[j,i] = medians[j,0] / medians[j,i]
+
+draw_scaling_graph(medians, "Runtimes on Subgraph Isomorphism si4_m4Dr2_m1296.06", 2, 10000000, "Runtime (s)")
+draw_scaling_graph(speedUps, "Scaling on Subgraph Isomporhism on si4_m4Dr2_m1296.06", 2, 10000000, "Relative SpeedUp (1 locality)")
+
 times, nodes, backtracks, sTimes, p = read_search_metrics("../b401_d.txt")
-draw_bucket_graph(times, "Runtime regularity on MaxClique brock400_1.clq, Depthbounded d = 2")
+draw_bucket_graph(times, "Runtime regularity on MaxClique brock400_1.clq, Depthbounded d = 2, 1 Locality", [i for i in range(3)])
+
+times, nodes, backtracks, sTimes, p = read_search_metrics("../b800_reg.txt")
+draw_bucket_graph(times, "Runtime regularity on MaxClique brock800_4.clq, Depthbounded d = 2, 1 Locality", [i for i in range(3)])
+
+times, nodes, backtracks, sTimes, p = read_search_metrics("../b800_reg_bug.txt")
+draw_bucket_graph(times, "Runtime regularity on MaxClique brock800_4.clq,Budget b = 10000000, 1 Locality", [i for i in range(7)])
+
+times, nodes, backtracks, sTimes, p = read_search_metrics("../rs_sip_d_6.txt")
+draw_bucket_graph(times, "Runtime regularity on Subgraph Isomporhism g34-g79, Depthbounded d = 6, 1 Locality", [i for i in range(7)])
+
+times, nodes, backtracks, sTimes, p = read_search_metrics("../rs_sip_stack.txt")
+draw_bucket_graph(times, "Runtime regularity on Subgraph Isomporhism g34-g79, Stacksteal, 1 Locality", [i for i in range(2)])
+
+times, nodes, backtracks, sTimes, p = read_search_metrics("../rs_sip_budg.txt")
+draw_bucket_graph(times, "Runtime regularity on Subgraph Isomporhism g34-g79, Budget b = 10000000, 1 Locality", [i for i in range(7)])
