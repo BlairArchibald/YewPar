8b50ad097f98f81aaeac2d2589275488bbb6c3e5
ruairidhm98 <2250079m@student.gla.ac.uk>
2020-07-05 14:27:19 +0100

    Moved results out of repo

--- YewPar_env.sh
+++
@@ -1,3 +0,0 @@
-export PATH=/users/level4/2250079m/sandbox/YewParInstall/cmake-3.14.4-Linux-x86_64/bin/:$PATH
-export LD_LIBRARY_PATH=/users/level4/2250079m/sandbox/YewParInstall/boost_1_70_0/stage/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/users/level4/2250079m/sandbox/YewParInstall/hpx-1.2.1/build/install/lib:$LD_LIBRARY_PATH
--- scripts/metrics.txx
+++
@@ -1,3 +0,0 @@
-CPU Time (Before collecting metrics) 131141
-MaxClique Size = 23
-cpu = 131221
